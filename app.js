a95b8a6a4d821777ad6347841552199b9bfbf80a
naima-shk <naimashaikh550@gmail.com>
2021-07-17 00:13:10 +0500

    'Implemented-arithmetic-calculator-using-dom'

---
+++ app.js
@@ -0,0 +1,53 @@
+/* pseudocode 
+1- Listen and capture all the button events.
+2-when an event occurs, depending on the button, i will do +,x,/,-
+3-For any operation, I have to get values from the text boxes too.
+*/
+
+//add button
+addButton = document.getElementById('addButton');
+addOperation =function(){
+    num1= document.getElementById('num1');
+    num2= document.getElementById('num2');
+
+    sum= parseInt(num1.value) + parseInt(num2.value);
+    result= document.getElementById('result');
+    result.innerText =sum;
+}
+addButton.addEventListener('click', addOperation);
+
+//subtract  button
+minusButton = document.getElementById('minusButton');
+minusOperation =function(){
+    num1= document.getElementById('num1');
+    num2= document.getElementById('num2');
+
+    difference= parseInt(num1.value) - parseInt(num2.value);
+    result= document.getElementById('result');
+    result.innerText = difference;
+}
+minusButton.addEventListener('click', minusOperation);
+
+//multiply button
+multiplyButton = document.getElementById('multiplyButton');
+multiplyOperation =function(){
+    num1= document.getElementById('num1');
+    num2= document.getElementById('num2');
+
+    product= parseInt(num1.value) * parseInt(num2.value);
+    result= document.getElementById('result');
+    result.innerText = product;
+}
+multiplyButton.addEventListener('click', multiplyOperation);
+
+//division button
+divButton = document.getElementById('divButton');
+divOperation =function(){
+    num1= document.getElementById('num1');
+    num2= document.getElementById('num2');
+
+    division= parseInt(num1.value) /  parseInt(num2.value);
+    result= document.getElementById('result');
+    result.innerText = division;
+}
+divButton.addEventListener('click', divOperation);
